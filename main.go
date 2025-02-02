c46e9daf6c214785b08a7f8c66cd1508e81f90fb
skeen0711 <skeen0711@gmail.com>
2025-02-01 23:03:21 -0600

    Adding ability to take multiple commands

--- main.go
+++ main.go
@@ -10,15 +10,20 @@ import (
 
 func main() {
 	fmt.Println("Coding Challenges: Build your own Shell in Go")
+	for {
+		fmt.Print("goShell> ")
 
-	fmt.Print("goShell> ")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			break
+		}
+		cmd := exec.Command(input)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
 
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
-
-	cmd := exec.Command(input)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
+		cmd.Wait()
+	}
 }
