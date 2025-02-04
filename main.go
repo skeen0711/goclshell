df21c01053a867c99b01d25f2008e0ebb358d271
skeen0711 <skeen0711@gmail.com>
2025-02-03 22:41:35 -0600

    Beginning to explore functionality for adding piping commands

--- main.go
+++ main.go
@@ -25,17 +25,54 @@ func main() {
 		var command = parts[0]
 		var args = parts[1:]
 
-		if command == "exit" {
+		switch command {
+		case "exit":
 			os.Exit(0)
-		}
 
-		cmd := exec.Command(command, args...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+		case "cd":
+			var path string
+			var err error
 
-		err := cmd.Run()
-		if err != nil {
-			fmt.Println(err)
+			if len(args) > 0 {
+				path = args[0]
+			} else {
+				path, _ = os.UserHomeDir()
+			}
+			err = os.Chdir(path)
+			if err != nil {
+				fmt.Printf("%v\n", err)
+			}
+
+		case "pwd":
+			dir, _ := os.Getwd()
+			fmt.Println(dir)
+
+		default:
+			cmdList := strings.Split(input, "|")
+			for _, cmd := range cmdList {
+				parts := strings.Fields(cmd)
+				pCommand := parts[0]
+				args = parts[1:]
+
+				pCommand = strings.TrimSpace(pCommand)
+				pArgs := strings.TrimSpace(strings.Join(args, " "))
+				fCom := exec.Command(pCommand, pArgs)
+				fCom.Stdout = os.Stdout
+				fCom.Stderr = os.Stderr
+
+				err := fCom.Run()
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			cmd := exec.Command(command, args...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 }
